324599098449fbb2e8747c484378ecdb78de42eb
TarcaAndreea <adrianatarca1@gmail.com>
2023-10-08 00:07:01 +0300

    update routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import { store } from 'Redux/store';
@@ -7,9 +7,10 @@ import HomePage from '../Pages/HomePage';
 import RegisterPage from '../Pages/RegisterPage';
 import Loginpage from '../Pages/LoginPage';
 import ContactsPage from '../Pages/ContactsPage';
-
+import NotFoundPage from 'Pages/NotFoundPage';
 import PrivateRoutes from './Routes/PrivateRoutes';
 import homepag from '../Images/homepag.jpg';
+
 export const App = () => {
   return (
     <Provider store={store}>
@@ -25,18 +26,16 @@ export const App = () => {
           height: '100vh',
         }}
       >
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
-          <Router>
-            <Routes>
-              <Route path="/" element={<HomePage />} />
-              <Route path="/register" element={<RegisterPage />} />
-              <Route path="/login" element={<Loginpage />} />
-
-              <Route element={<PrivateRoutes />}>
-                <Route path="/contacts" element={<ContactsPage />} />
-              </Route>
-            </Routes>
-          </Router>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="/login" element={<Loginpage />} />
+            <Route element={<PrivateRoutes />}>
+              <Route path="/contacts" element={<ContactsPage />} />
+            </Route>
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
         </BrowserRouter>
       </div>
     </Provider>
